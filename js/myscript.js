dfa0e5ad28c1eedace6b108da7bcd975ca28249d
Sirio <sirio.iasevoli@gmail.com>
2022-11-04 16:58:34 +0100

    password super sicura

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+const name = prompt("Scrivi il tuo nome");
+const surname = prompt("Scrivi il tuo cognome");
+const color =prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("password").innerHTML = `La tua password SUPER sicura è: ${name}${surname}${color}21`
